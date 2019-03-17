ca23751eee70d5e2e581f7a15e3853ffe7cd075c
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2019-03-17 14:38:41 +0100

    add http

---
+++ sensors-backend/src/main/java/pl/tomek/sensors/sensorsbackend/Application.java
@@ -0,0 +1,35 @@
+package pl.tomek.sensors.sensorsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableAsync
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("POST", "GET", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ sensors-backend/src/main/java/pl/tomek/sensors/sensorsbackend/controller/HelloController.java
@@ -0,0 +1,19 @@
+package pl.tomek.sensors.sensorsbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/hello")
+public class HelloController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Object helloWorld() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("text", "Hello World2!");
+        return result;
+    }
+}
