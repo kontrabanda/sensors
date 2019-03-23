d33fa3d9c81380eafc000c6e87788d43cfc24449
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2019-03-23 10:14:34 +0100

    add .gitignore

--- sensors-hardware/sensors-hardware.ino
+++ sensors-hardware/sensors-hardware.ino
@@ -2,10 +2,7 @@
 #include "SensorsMessage.h"
 #include "DS18B20AndDHTMessage.h"
 
-char* ssid     = "*"; // Tu wpisz nazwę swojego wifi
-char* password = "*"; // Tu wpisz hasło do swojego wifi
-int dhtPin = D2;
-int ds18b20Pin = D1;
+#include "MainConst.h"
 
 DS18B20AndDHTMessage sensorsMessage(dhtPin, ds18b20Pin);
 SensorsServer sensorsServer(ssid, password, &sensorsMessage);
